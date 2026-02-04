949199522d1192eecdef405053ae862211bfee2d
Kundan-Rawal <rawalkundan987@gmail.com>
2026-02-04 13:39:44 +0530

    Added required backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/", // Check your backend port!
+  baseURL: "https://todoassignmentalt.onrender.com", // Check your backend port!
   withCredentials: true, // CRITICAL: Allows cookies to be sent/received
 });
 
